18aab68561903cd240abb125068a661035374d8f
ntanners <nadavtanners@gmail.com>
2018-05-08 05:57:33 -0400

    completed mySQL->ES import process

--- mySQL_connect.py
+++ mySQL_connect.py
@@ -5,7 +5,9 @@ import time
 QUERIES = {
     'describe': """DESCRIBE {}""",
     'count': """SELECT COUNT(*) FROM {}""",
-    'table_list': """SHOW TABLES"""
+    'table_list': """SHOW TABLES""",
+    'contents': """SELECT * FROM {}""",
+    'contents_limit': """SELECT * FROM {} LIMIT {}"""
 }
 
 
@@ -54,21 +56,46 @@ def create_table(cur, tbl_name, tbl_schema):
             (", ".join(" ".join(row) for row in tbl_schema)) + ")"
     cur.execute(query)
 
-def run_fetch_query(cur, keyword):
-    if keyword == 'count' or keyword == 'describe':
-        query = QUERIES[keyword].format(sys.argv[2])
+def run_fetch_query(cur, keyword, table=None, limit=None):
+    if keyword == 'contents_limit':
+        query = QUERIES[keyword].format(table, limit)
+    elif keyword == 'count' or keyword == 'describe' or keyword == 'contents':
+        query = QUERIES[keyword].format(table)
     elif keyword == 'list':
         query = QUERIES['table_list']
     else:
         raise "not a valid query"
-    cur.execute(query)
-    return cur
+    numrows = cur.execute(query)
+    return numrows, cur
 
-def time_query(cur, keyword):
+def run_select_query(cur, table, batchsize):
+    cur.execute("""SELECT COUNT(*) FROM {}""".format(table))
+    nrows = cur.fetchone()[0]
+    start_point = 0
+    results = []
+    while nrows > 0:
+        print('retrieving records {} to {}'.format(start_point, start_point + min(nrows, batchsize)))
+        cur.execute("""SELECT * FROM {} ORDER BY 1 LIMIT {},{}""".format(table,start_point,min(nrows,batchsize)))
+        # results = results + list(cur.fetchone())
+        print(cur.fetchone()[0])
+        nrows -= batchsize
+        start_point += batchsize
+    # print(len(results))
+
+def run_query(cur, query):
+    nrows = cur.execute(query)
+    return nrows, cur
+
+def get_colnames(cur, table):
+    n, cur = run_fetch_query(cur, 'describe', table)
+    cols = cur.fetchall()
+    return [col[0] for col in cols]
+
+def time_query(cur, query):
     start_time = time.time()
-    cur = run_fetch_query(cur, keyword)
+    n, cur = run_query(cur, query)
     end_time = time.time()
-    return cur, end_time-start_time
+    return n, cur, end_time-start_time
 
 
 def main():
@@ -78,9 +105,34 @@ def main():
     # tbl_name = 'doctor'
     # tbl_schema = schemas[tbl_name]
     # create_table(cur, 'doctor_new', tbl_schema)
-    cur, runtime  = time_query(cur, sys.argv[1])
-    print('the query took {:.2f} seconds, and the result was:'.format(runtime))
-    print(cur.fetchall())
+
+    # cur.execute("""SET net_read_timeout=28800""")
+    # cur.execute("""SET GLOBAL connect_timeout=600""")
+
+    n, cur, time = time_query(cur, sys.argv[1])
+    print(n, cur.fetchone(), time)
+    # run_select_query(cur, 'ee_audit_events_orig', 200000)
+    # keyword = sys.argv[1]
+    # table, limit = None, None
+    # if len(sys.argv)>2:
+    #     table = sys.argv[2]
+    # if len(sys.argv)>3:
+    #     limit = sys.argv[3]
+    # n, cur, runtime  = time_query(cur, keyword, table, limit)
+    # print('the query took {:.2f} seconds, and the result was:'.format(runtime))
+    # if n > 100:
+    #     print(n, 'rows')
+    # else:
+    #     print(cur.fetchall())
+
+    # cur.execute("""SHOW VARIABLES LIKE '%TIME%'""")
+    # cur.execute("""SELECT @@MAX_ALLOWED_PACKET""")
+    # print(cur.fetchall())
+    # if sys.argv[1] == 'cols':
+    #     result = get_colnames(cur, sys.argv[2])
+    # else:
+    #     _, result = run_fetch_query(cur, sys.argv[1])
+    # print(result.fetchall())
     close_connection(con, cur)
 
 if __name__ == '__main__':
