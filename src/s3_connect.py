2425228096f0d0ed0624425843609ff3b57a5b34
ntanners <nadavtanners@gmail.com>
2018-05-22 15:43:01 -0400

    added argparser CLI

--- src/s3_connect.py
+++ src/s3_connect.py
@@ -1,38 +1,15 @@
-import boto
 from boto.s3.connection import S3Connection
-import boto3, botocore
+from argparse import ArgumentParser
 from mySQL_connect import load_connection_info
 
-s3_info = load_connection_info('./login/.aws', [])
-AWS_ACCESS_KEY_ID = s3_info['access_key']
-AWS_SECRET_ACCESS_KEY = s3_info['secret_key']
+LOGIN_PATH = '../login/.aws'
 
-def boto3_connection():
-    s3_info = load_connection_info('./login/.aws', [])
-    AWS_ACCESS_KEY_ID = s3_info['access_key']
-    AWS_SECRET_ACCESS_KEY = s3_info['secret_key']
+
+def connect_to_s3(access_key, secret_key):
+    """Creates a connection to S3.
+    Inputs: AWS primary key and access key"""
     try:
-        s3_sess = boto3.resource('s3')\
-            # , aws_access_key_id = s3_info['access_key'],
-            #                      aws_secret_access_key = s3_info['secret_key'])
-        print('session created successfully')
-    except Exception as e:
-        print(e)
-    # try:
-    #     s3_cl = boto3.client('s3')\
-    #         # , aws_access_key_id = s3_info['access_key'],
-    #         #                      aws_secret_access_key = s3_info['secret_key'])
-    #     response = s3_cl.list_buckets()
-    #     print(response['Buckets'])
-    except Exception as e:
-        print(e)
-
-
-
-def connect_to_s3(s3_info):
-    """Inputs: dictionary with AWS primary key and access key"""
-    try:
-        s3con = S3Connection(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
+        s3con = S3Connection(access_key, secret_key)
         print('Connection successful')
         return s3con
     except Exception as e:
@@ -40,21 +17,24 @@ def connect_to_s3(s3_info):
 
 
 def list_buckets(s3con):
+    """Lists all of the buckets in an S3 connection"""
     listbuckets = s3con.get_all_buckets()
     print(listbuckets)
 
 
 def list_bucket_contents(s3con, bucket):
+    """Lists all of the contents of an S3 bucket"""
     bucket = s3con.get_bucket(bucket)
     contents = bucket.list()
     for item in contents:
         print(item)
 
-def get_file(s3con, bucketname, folder, filename):
-    bucket = s3con.get_bucket(bucketname)
-    # path = '/' + folder + '/' + filename
-    # print(path)
-    key = bucket.get_key('/' + folder + '/' + filename)
+
+def get_file(s3con, bucket, folder, filename):
+    """Downloads a file from an S3 bucket into the local directory
+    Inputs: connection, bucket name, folder (full path) in the bucket, filename"""
+    bucket_obj = s3con.get_bucket(bucket)
+    key = bucket_obj.get_key('/' + folder + '/' + filename)
     key.get_contents_to_filename(filename)
 
 
@@ -64,21 +44,41 @@ def close_s3_connection(con):
 
 
 def __main__():
-    s3_info = load_connection_info('./login/.aws', [])
-    s3con = connect_to_s3(s3_info)
-    list_bucket_contents(s3con, 'nt-augmedix-demo')
-    get_file(s3con, 'nt-augmedix-demo', 'new-folder', 'ee_log.csv')
+    # Connect to S3
+    s3_info = load_connection_info(LOGIN_PATH, [])
+    s3con = connect_to_s3(s3_info['access_key'], s3_info['secret_key'])
+
+    # Use ArgumentParser to parse command line arguments
+    parser = ArgumentParser(description='Augmedix project S3 functions CLI')
+    subparser_base = parser.add_subparsers(title='actions', description='Choose an action')
+
+    sp = subparser_base.add_parser('list_buckets')
+    sp.set_defaults(which='list_buckets')
+
+    sp = subparser_base.add_parser('bucket_contents')
+    sp.set_defaults(which='bucket_contents')
+    sp.add_argument('-b', '--bucket', default='nt-augmedix-demo', help='bucket to list contents for')
+
+    sp = subparser_base.add_parser('get_file')
+    sp.set_defaults(which='get_file')
+    sp.add_argument('-b', '--bucket', default='nt-augmedix-demo', help='bucket to download file from')
+    sp.add_argument('-f', '--folder', default='new-folder', help='full path of folder where file lies')
+    sp.add_argument('-n', '--filename', default='doctor.csv', help='name of file to be downloaded')
+
+    args = vars(parser.parse_args())
+    args['s3con'] = s3con
+    action = args['which']
+    del args['which']
+
+    if action == 'list_buckets':
+        list_buckets(**args)
+    elif action == 'bucket_contents':
+        list_bucket_contents(**args)
+    elif action =='get_file':
+        get_file(**args)
+
     close_s3_connection(s3con)
 
+
 if __name__ == '__main__':
-    # boto3_connection()
     __main__()
-
-    # bucket = con.get_bucket('nt-augmedix-demo')
-    # try:
-    #     listbuckets = con.get_all_buckets()
-    #     print(listbuckets)
-    # finally:
-    #     close_s3_connection(con)
-    # print(bucket.list())
-
